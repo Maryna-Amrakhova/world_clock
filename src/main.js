ed92dc246908073ee3367339b563820e88568ced
Maryna-Amrakhova <seagull2305@gmail.com>
2024-09-12 16:27:08 +0300

    added multiple locations

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 setInterval(function () {
 
-
     // New York
     let newYorkElement = document.querySelector("#new-york");
     let newYorkDateElement = newYorkElement.querySelector(".date");
@@ -24,4 +23,24 @@ setInterval(function () {
     let londonTime = moment().tz("Europe/London");
     londonDateElement.innerHTML = londonTime.format("MMMM Do, YYYY");
     londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+
 }, 1000);
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+                <div>
+                    <h2>${cityName}</h2>
+                    <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+                </div>
+                <div class="time"> ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>                   
+                </div>
+            </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
